f46e40520b135fe01a026a8d5ba7c16c607b63bf
Elton.Wang <elton.wang@wpgholdings.com>
2022-11-17 11:47:06 +0800

    add ArgumentParser

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from pypinyin import Style, pinyin
+from argparse import ArgumentParser
 
+parser = ArgumentParser()
+parser.add_argument("-t", "--text", type=str,
+                    help="請輸入需要轉換的中文", required=True)
+args = parser.parse_args()
 
 # 注音對照表
 BOPOMOFO_TABLE = dict(zip(
@@ -20,4 +25,4 @@ def bopomofo_convert(raw_input):
 
 if __name__ == '__main__':
     # '我的密碼' -> 'ji32k7au4a83'
-    print(bopomofo_convert('我的密碼')) 
+    print(bopomofo_convert(args.text)) 
